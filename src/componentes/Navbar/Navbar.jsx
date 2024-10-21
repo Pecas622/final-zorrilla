c4c8ec528d40c7f461eadbef87e21f21f64ddd74
Pecas622 <santiagozorrilla5@gmail.com>
2024-10-21 16:07:48 -0300

    nav

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CartWidget from '../CartWidget/CartWidget';
-import './NavBar.css';
+import "./NavBar.css";
 import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { getDocs, collection } from 'firebase/firestore';
